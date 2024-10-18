a2360ff6dd2624fb301663fb997dbde28124b249
Simon Le Marchant <simon@marchantweb.com>
2024-10-17 20:58:31 -0400

    fix: updated playground to use actual component

--- playground/app.vue
+++ playground/app.vue
@@ -1,60 +1,20 @@
 <template>
   <!-- Page Navigation -->
-  <NavigationButtons />
+  <NavigationButtons/>
 
   <!-- Teleport the "Subject" component to the target CSS selector inside the page -->
-  <client-only>
-    <Teleport
-      v-show="isTargetPresent"
-      :to="targetId"
-    >
-      <ExampleComponent />
-    </Teleport>
-  </client-only>
+  <AdaptiveTeleport
+    to="#target-5"
+  >
+    <ExampleComponent/>
+  </AdaptiveTeleport>
 
   <!-- Page content -->
-  <NuxtPage />
+  <NuxtPage/>
 </template>
 
 <script setup lang="ts">
-import { ref, watch, onMounted, onUnmounted } from 'vue'
-import { useMutationObserver } from '@vueuse/core'
 
-// Default the <Teleport> target to the SSR-friendly '#teleports' element
-const targetId = ref('#teleports')
-const isTargetPresent = ref(false)
-
-const checkTargetPresence = () => {
-  const target = document.querySelector('#target-5')
-  isTargetPresent.value = !!target
-}
-
-onMounted(() => {
-  // Initial check
-  checkTargetPresence()
-
-  // Set up mutation observer
-  const observer = useMutationObserver(document.body, () => {
-    checkTargetPresence()
-  }, {
-    childList: true,
-    subtree: true,
-  })
-
-  // Clean up observer on component unmount
-  onUnmounted(() => {
-    observer.stop()
-  })
-})
-
-// Watch for changes in target presence and the page route
-const route = useRoute()
-watch([isTargetPresent, route], () => {
-  targetId.value = '#teleports'
-  nextTick(() => {
-    targetId.value = isTargetPresent.value ? '#target-5' : '#teleports'
-  })
-})
 </script>
 
 <style>
